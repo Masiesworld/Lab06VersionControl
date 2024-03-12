f0625eff0942732df1759de00b59cd6968e57182
Masiesworld <ymxmacy@gmail.com>
2024-03-12 15:17:56 -0400

    Add Encode and main file by Macy Yu

---
+++ main.py
@@ -0,0 +1,32 @@
+'''Encode part: Macy Yu'''
+def print_menu():
+    print('Menu')
+    print('-------------')
+    print('1. Encode \n2. Decode \n3. Quit\n')
+
+
+def encoder(string):
+    res = []
+    for i in string:
+        res.append(str(int(i) + 3))
+    res = ''.join(res)
+    return res
+
+
+def main():
+    while True:
+        print_menu()
+        user_choice = int(input('Please enter an option: '))
+        if user_choice == 1:
+            user_string = input('Please enter your password to encode: ')
+            print('Your password has been encoded and stored!\n')
+
+        elif user_choice == 2:
+            decoded_string = encoder(user_string)
+            print(f'The encoded password is {user_string}, and the original password is {decoded_string}.')
+
+        elif user_choice == 3:
+            break
+
+if __name__ == '__main__':
+    main()
